77026fd0525eaefe6764146d19660df96fb592e3
gbenetatosprsn <153236276+gbenetatosprsn@users.noreply.github.com>
2024-08-06 03:35:19 -0500

    Create locals.tf

---
+++ locals.tf
@@ -0,0 +1,10 @@
+locals {
+  coid           = "rush"
+  environment    = "p"
+  location       = "centralus"
+  location_short = "cus"
+  function       = "hub"
+
+  hub_address_space         = [""]
+  virtual_wan_address_space = [""]
+}
