c92c7ddf49b7ef5b9239465dd29b90ecaff5b5c9
gbenetatosprsn <153236276+gbenetatosprsn@users.noreply.github.com>
2024-08-06 03:53:28 -0500

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -5,6 +5,6 @@ locals {
   location_short = "cus"
   function       = "hub"
 
-  hub_address_space         = [""]
-  virtual_wan_address_space = [""]
+  hub_address_space         = ["192.168.150.0/24"]
+  virtual_wan_address_space = ["192.168.160.0/23"]
 }
