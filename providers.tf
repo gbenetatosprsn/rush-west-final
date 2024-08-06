095a33b8033c409d9f73042e1f63fbaf3fcdc307
gbenetatosprsn <153236276+gbenetatosprsn@users.noreply.github.com>
2024-08-06 03:52:29 -0500

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,1 +1,17 @@
-providers
+terraform {
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.60.0"
+    }
+  }
+}
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+}
